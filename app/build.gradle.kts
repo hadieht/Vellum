4e4f51ce35d66e6bf8731f52cc9ad193d34a8016
Hadi <hadi.ehterami@gmail.com>
2026-05-31 21:20:01 +0200

    rename project name to Koda

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.ehterami.Koda"
         minSdk = 30
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
