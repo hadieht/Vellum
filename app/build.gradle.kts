dd9d991b9026a213cd8a9ba80fcaaf4543c4706a
Hadi <hadi.ehterami@gmail.com>
2026-05-31 23:06:33 +0200

    upgrade version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.ehterami.vellum"
         minSdk = 30
         targetSdk = 37
-        versionCode = 4
-        versionName = "2.1"
+        versionCode = 5
+        versionName = "2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
