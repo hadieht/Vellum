83e229ae0419c9636be5d11764c24301a15ed312
Hadi <hadi.ehterami@gmail.com>
2026-05-31 22:50:27 +0200

    add new colors to app, replace old app name remaining

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,15 +13,16 @@ android {
         applicationId = "com.ehterami.vellum"
         minSdk = 30
         targetSdk = 37
-        versionCode = 3
-        versionName = "2.0"
+        versionCode = 4
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
